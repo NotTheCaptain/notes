43d9fdf6d7930bd8b19fdf5e788b94fe770211f9
NotTheCaptain <stefan.krottendorfer@outlook.at>
2022-09-28 21:00:01 +0200

    Step 13: Implementing new model but cryptojs doesnt work local

--- index.js
+++ index.js
@@ -2,11 +2,27 @@
 const notes = [];
 
 // View
-function buildLIItem(text) {
-  const note = document.createElement("li");
-  note.innerText = text;
-  note.addEventListener("click", handleClickLIItem);
-  return note;
+function buildLIItem(note) {
+  const item = document.createElement("li");
+  item.id = note.id;
+
+  const article = document.createElement("article");
+  const title = document.createElement("header");
+  title.textContent = note.title;
+  const text = document.createElement("p");
+  text.textContent = note.text;
+
+  const controls = document.createElement("div");
+  const button = document.createElement("button");
+  button.textContent = "Delete";
+  button.addEventListener("click", handleClickDelete(note.id));
+  controls.appendChild(button);
+
+  article.appendChild(title);
+  article.appendChild(text);
+  article.appendChild(controls);
+  item.appendChild(article);
+  return item;
 }
 
 // Controller
@@ -19,20 +35,38 @@ function handleClick() {
   add();
 }
 
-function handleClickLIItem(event) {
-  const list = document.getElementById("list");
-  list.removeChild(event.target);
+function handleClickDelete(id) {
+  return function () {
+    const item = document.getElementById(id);
+    const list = document.getElementById("list");
+    list.removeChild(item);
+    const pos = notes.findIndex((note) => note.id === id);
+    notes.splice(pos, 1);
+  };
 }
 
 function add() {
-  const input = document.getElementById("text");
-  const text = input.value;
-  if (text) {
-    let ul = document.getElementById("list");
-    const note = buildLIItem(text);
-    ul.appendChild(note);
+  const title = document.getElementById("title");
+  const text = document.getElementById("text");
+  if (title.value || text.value) {
+    const list = document.getElementById("id");
+    const note = createNote(title.value, text.value);
+    const item = buildLIItem(note);
+    list.appendChild(item);
     notes.push(note);
-    input.value = "";
-    input.focus();
+    title.value = "";
+    text.value = "";
   }
+}
+
+function createNote(title, text) {
+  const id = generateId(title, text);
+  return { id, title, text };
+}
+
+function generateId(title, text, length = 10) {
+  return CryptoJS.SHA256(title * text * new Date()).toString.substring(
+    0,
+    length
+  );
 }
