eb1980764be6c4cdf7adac84acb2491d00d09bd6
NotTheCaptain <stefan.krottendorfer@outlook.at>
2022-09-28 19:40:07 +0200

    Step 8: adding basic delete function, eventlistener for notes

--- index.js
+++ index.js
@@ -11,15 +11,21 @@ function handleKeyDown(event) {
   }
 }
 
+function handleClickLIItem(event) {
+  const list = document.getElementById("list");
+  list.removeChild(event.target);
+}
+
 function add() {
   const input = document.getElementById("text");
   const text = input.value;
   if (text) {
-    let ul = document.querySelector("ul");
-    let li = document.createElement("li");
+    let ul = document.getElementById("list");
+    let note = document.createElement("li");
 
-    li.innerText = text;
-    ul.appendChild(li);
+    note.innerText = text;
+    note.addEventListener("click", handleClickLIItem);
+    ul.appendChild(note);
 
     input.value = "";
     input.focus();
